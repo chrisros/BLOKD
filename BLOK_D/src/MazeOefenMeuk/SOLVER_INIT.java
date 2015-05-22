6a9a83c61f624678b169b74291dcc11fd66a3e09
chrisros <chris.ros@mylcros.nl>
2015-05-22 12:54:29 +0200

    BUILD vergadering

--- BLOK_D/src/MazeOefenMeuk/SOLVER_INIT.java
+++ BLOK_D/src/MazeOefenMeuk/SOLVER_INIT.java
@@ -1,9 +1,11 @@
 package MazeOefenMeuk;
 
+import java.io.IOException;
+
 
 public class SOLVER_INIT {
     
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
     Spel game = new Spel();
     game.constructLevel();    
     }
